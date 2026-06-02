218d3dda965116feb423dc7bc48bc4cc0a316171
lucid271 <lucidnyeredzi37@gmail.com>
2026-06-02 15:40:24 +0200

    Update qr.js

--- qr.js
+++ qr.js
@@ -20,7 +20,7 @@ function removeFile(FilePath) {
 router.get('/', async (req, res) => {
     const id = makeid();
  //   let num = req.query.number;
-    async function MALVIN_XD_PAIR_CODE() {
+    async function ICON-X MD_PAIR_CODE() {
         const {
             state,
             saveCreds
